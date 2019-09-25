87b7e7184661e3a92cedd29af781c8ec06e0a957
imxlss <imxlss@163.com>
2019-09-25 14:37:07 +0800

    fix: db test -> minesweeper

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost:27017/test', {
+  .connect('mongodb://localhost:27017/minesweeper', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
